41f365f9e64754c31632c4ce1b6f18b1df867ad8
aliah1105 <aliahadi.f1105@gmail.com>
2024-10-19 22:17:13 +0330

    edit some problem in database module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigModule as NestConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '../config';
 
 @Module({
-    imports: [MongooseModule.forRootAsync({
-        imports: [ConfigModule],
-        useFactory: (configService: ConfigService) => ({
-            uri: configService.get('MONGODB_URL'),
+    imports: [
+        NestConfigModule.forRoot({
+            isGlobal: true
         }),
-        inject: [ConfigService]
-    })]
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get('MONGODB_URL'),
+            })
+        })]
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
